3fb7332807bb3ede038a9d75e02ebbb182580329
red-135 <red-135@hotmail.com>
2016-03-08 11:13:30 -0600

    Add informational messages to main() to tell user what is happening in each step.

--- main.cpp
+++ main.cpp
@@ -76,16 +76,22 @@ int main(int argc, char** argv)
 	 return -1;
 	}
 
+	std::cout << "Capturing angle of line..." << std::endl;
+	std::cout << "Please click twice to set angle then press any button to continue." << std::endl;
+
 	cv::imshow("dir", image_src);
 	cv::setMouseCallback("dir", onClick);
 	cv::waitKey(0);
 	cv::destroyAllWindows();
+
+	std::cout << "Finding power lines..." << std::endl;
 
 	cv::Mat line_in = image_src.clone();
 	cv::Mat line_out = image_src.clone();
 
 	PowerLineDetection(line_in, line_out, 0.5, 10, 200, angle);
 
+	std::cout << "Finding hot spots..." << std::endl;
 
 	cv::Mat hot_inout = image_src.clone();
 	{
@@ -99,28 +105,37 @@ int main(int argc, char** argv)
 
 	hotSpotDetectionAlgorithm(hot_inout, contours, pixel_thresh);
 
+	std::cout << "Finding hot spots on power lines..." << std::endl;
 
 	cv::Mat conjunction;
 	cv::bitwise_and(line_out, hot_inout, conjunction);
 	for(int i = 0; i < 3; i++)
 			cv::dilate(conjunction, conjunction,cv::Mat());
 
+	std::cout << "Displaying power lines and hot spots..." << std::endl;
+	std::cout << "Please press 'q' to continue." << std::endl;
+
 	cv::imshow("src", image_src);
 	cv::imshow("line", line_out);
 	cv::imshow("hot", hot_inout);
 	cv::imshow("conj", conjunction);
-
 	while(char(cv::waitKey(1)) != 'q'){}
 	cv::destroyAllWindows();
+
+	std::cout << "Analyzing hot spots and power lines..." << std::endl;
 
 	cv::Mat decision_out;
 	Decision(line_out, hot_inout, image_src, decision_out);
 
+	std::cout << "Displaying analysis..." << std::endl;
+	std::cout << "Please press 'q' to continue." << std::endl;
+
 	cv::imshow("src", image_src);
 	cv::imshow("decision", decision_out);
-
 	while(char(cv::waitKey(1)) != 'q'){}
 	cv::destroyAllWindows();
+
+	std::cout << "Quitting..." << std::endl;
 
 	return 0;
 }
